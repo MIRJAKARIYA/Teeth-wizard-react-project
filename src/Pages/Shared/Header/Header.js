e4b18bca5fdf5fbc711f8eedd149e82a2eb69004
MIRJAKARIYA <mirjakariya239@gmail.com>
2022-04-17 02:23:21 +0600

    small change in navbar

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <Navbar collapseOnSelect expand="lg" className="navbar-style" variant="dark">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand as={Link} to='/home'>React-Bootstrap</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ms-auto">
