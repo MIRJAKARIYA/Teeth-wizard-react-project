8363f3be2bff3e7a67695e08ca5d3d1ac96f2b31
MIRJAKARIYA <mirjakariya239@gmail.com>
2022-04-17 12:53:27 +0600

    show more button style changed

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,36 +1,40 @@
-import React, { useEffect, useState } from 'react';
-import Banner from '../Banner/Banner';
-import './Home.css';
-import useServices from '../../../useServices';
-import SingleService from '../../Shared/SingleService/SingleService';
-import './Home.css';
+import React, { useEffect, useState } from "react";
+import Banner from "../Banner/Banner";
+import "./Home.css";
+import useServices from "../../../useServices";
+import SingleService from "../../Shared/SingleService/SingleService";
+import "./Home.css";
+import { BsArrowRight, BsArrowLeft } from "react-icons/bs";
+
 const Home = () => {
-    const services = useServices();
-    const [showAll, setShowAll] = useState(false);
-    const [showData, setShowData] = useState([]);
-    useEffect(()=>{
-        if(showAll){
-            setShowData(services)
-        }
-        else{
-            setShowData(services.slice(0,4))
-        }
-    },[showAll,services])
-    console.log(services);
-    return (
-        <div>
-            <Banner></Banner>
-            <div className='services-container'  id='services'>
-                <h1 className='text-center mb-5'>Services</h1>
-                <div className='row gy-4'>
-                {
-                    showData.map(service => <SingleService key={service.id} service={service}></SingleService>)
-                }
-                </div>
-                <button className='show-button' onClick={()=> setShowAll(!showAll)}>{showAll?'Show less': 'Show more'}</button>
-            </div>
+  const services = useServices();
+  const [showAll, setShowAll] = useState(false);
+  const [showData, setShowData] = useState([]);
+  useEffect(() => {
+    if (showAll) {
+      setShowData(services);
+    } else {
+      setShowData(services.slice(0, 4));
+    }
+  }, [showAll, services]);
+  console.log(services);
+  return (
+    <div>
+      <Banner></Banner>
+      <div className="services-container" id="services">
+        <h1 className="text-center mb-5">Services</h1>
+        <div className="row gy-4">
+          {showData.map((service) => (
+            <SingleService key={service.id} service={service}></SingleService>
+          ))}
         </div>
-    );
+        <button className="show-button" onClick={() => setShowAll(!showAll)}>
+          <span className="me-1">{showAll ? "Show less" : "Show more"}</span>
+          {showAll ? <BsArrowLeft /> : <BsArrowRight />}
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
